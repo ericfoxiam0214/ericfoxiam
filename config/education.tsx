3e37073751abaae640a2daf5158692f6b86c3c08
ericfoxiam <ericfoxiam0214@gmail.com>
2026-09-04 16:54:44 -0400

    deleted: test

--- config/education.tsx
+++ config/education.tsx
@@ -40,28 +40,6 @@ export const Educations: {
       </>,
     ],
   },
-  Test: {
-    name: 'Test',
-    longName: 'Stanford University',
-    url: 'https://www.stanford.edu/',
-    position: 'Bachelor of Computer Science',
-    duration: 'Sep 2012 - Nov 2016',
-    logo: {
-      light: '/worked_at_logos/STANFORD/STANFORD_light.png',
-      dark: '/worked_at_logos/STANFORD/STANFORD_dark.png',
-    },
-    roles: [
-      <>
-        Studied ML(machine learning), AI(artificial intelligence), web development area
-      </>,
-      <>
-        Researched ML-translation engine with corpus database written by Java and Python
-      </>,
-      <>
-        Rebuilt web application for school community platform
-      </>,
-    ],
-  },
 }
 
 export const EducationsList = [
